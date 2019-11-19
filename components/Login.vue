dd7f5570dd869faa582a60191ada6abf159c1558
bmut <bmut@refineriaweb.com>
2019-11-19 15:25:09 +0100

    Git init

---
+++ components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="o-wrapper o-wrapper--l">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    title: {
+      type: String,
+      default: 'caracol'
+    }
+  }
+}
+</script>
