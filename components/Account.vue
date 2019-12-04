55e9294dca7d12bd73b7621d4f302e2dc8415355
pcoll.refineriaweb <pcoll@refineriaweb.com>
2019-12-04 13:49:47 +0100

    Created Account page

---
+++ components/Account.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="u-custom-wrapper">
+    <Sidebar />
+    <Main />
+  </div>
+</template>
+
+<script>
+import Sidebar from '~/components/site/Sidebar'
+import Main from '~/components/account/Main'
+
+export default {
+  name: 'Home',
+  components: {
+    Sidebar,
+    Main
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.u-custom-wrapper {
+  display: flex;
+  width: 100%;
+}
+</style>
