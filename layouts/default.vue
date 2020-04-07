2d65d7ae417599f876d0ff6ac301aeaabc469315
David Oliva <doliva@refineriaweb.com>
2020-04-07 11:23:45 +0200

    Refactor

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+import { firebase } from '~/plugins/firebase'
+
 export default {
   data() {
     return {
@@ -29,6 +31,35 @@ export default {
       right: true,
       rightDrawer: false,
       title: 'Vuetify.js'
+    }
+  },
+  mounted() {
+    this.createPost()
+  },
+  methods: {
+    async createPost() {
+      const postData = {
+        author: 'me',
+        uid: 'uid1',
+        body: 'body1',
+        title: 'title1',
+        starCount: 0
+      }
+      const newPostKey = firebase
+        .database()
+        .ref()
+        .child('posts')
+        .push().key
+      const uid = 'ubc9fiKjZacO1SbmFNmYzBylGIT2' // user id
+      const updates = {}
+      updates['/posts/' + newPostKey] = postData
+      updates['/user-posts/' + uid + '/' + newPostKey] = postData
+      // Update database
+      await true
+      // await firebase
+      //   .database()
+      //   .ref()
+      //   .update(updates)
     }
   }
 }
