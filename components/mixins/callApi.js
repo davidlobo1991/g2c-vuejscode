7375688fe4f2e70017f0202ed0f176501b559e46
David Gonzalez | Refineria Web <dgonzalez@refineriaweb.com>
2020-06-17 17:03:21 +0200

    Fix call api

--- components/mixins/callApi.js
+++ components/mixins/callApi.js
@@ -1,28 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 const apiBackend = {
-  mounted() {
-    this.$axios.defaults.headers.post['Content-Type'] =
-      'application/x-www-form-urlencoded'
-    this.$axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
-  },
   methods: {
     getCallApi(url, data = null) {
-      return this.$axios
-        .get(url + data, {
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          }
-        })
-        .then((response) => response.data)
+      return this.$axios.get(url + data).then((response) => response.data)
     },
     postCallApi(url, data = null) {
-      return this.$axios
-        .post(url, data, {
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          }
-        })
-        .then((response) => response.data)
+      return this.$axios.post(url, data).then((response) => response.data)
     },
     putCallApi(url, data = null) {
       return this.$axios.put(url, data).then((response) => response.data)
