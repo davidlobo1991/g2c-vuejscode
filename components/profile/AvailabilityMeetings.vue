d963874744c99bccb4a733408ad217f76880fbdd
pcoll.refineriaweb <pcoll@refineriaweb.com>
2020-06-29 14:33:03 +0200

    Update frontend

---
+++ components/profile/AvailabilityMeetings.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="emeetings-avail">
+    <div class="emeetings-avail--top-cont">
+      <div class="emeetings-avail--title">eMeetings Availability</div>
+    </div>
+    <div class="emeetings-avail--cont">
+      <div class="emeetings-avail__elem">
+        <div class="emeetings-avail__elem--date">
+          <span class="emeetings-avail--bold">8</span> APR, TH
+        </div>
+        <div class="emeetings-avail__elem--hours-cont">
+          <ul class="emeetings-avail__elem--hours-list">
+            <li class="emeetings-avail__elem--hours-elem">9:00 - 11:30</li>
+            <li class="emeetings-avail__elem--hours-elem">20:00 - 22:30</li>
+          </ul>
+        </div>
+      </div>
+      <div class="emeetings-avail__elem">
+        <div class="emeetings-avail__elem--date">
+          <span class="emeetings-avail--bold">8</span> APR, TH
+        </div>
+        <div class="emeetings-avail__elem--hours-cont">
+          <ul class="emeetings-avail__elem--hours-list">
+            <li class="emeetings-avail__elem--hours-elem">9:00 - 11:30</li>
+            <li class="emeetings-avail__elem--hours-elem">20:00 - 22:30</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvailabilityMeetings',
+  data: () => ({}),
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.emeetings-avail {
+  position: relative;
+  border-radius: 4px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  &--top-cont {
+    width: 100%;
+    padding: 20px;
+    border-bottom: solid 1px #000;
+  }
+  &--title {
+    color: #21273b;
+    font-size: 19px;
+    font-weight: 500;
+  }
+  &--bold {
+    font-weight: bold;
+  }
+  &--cont {
+  }
+  &__elem {
+    padding: 20px;
+    border-bottom: solid 1px #000;
+    display: grid;
+    grid-template-columns: 2fr 5fr;
+    &:last-of-type {
+      border: none;
+    }
+    &--hours-cont {
+    }
+    &--hours-list {
+      list-style: none; /* Remove default bullets */
+      padding: 0;
+    }
+    &--hours-elem {
+      display: flex;
+      align-items: center;
+    }
+    &--hours-list .emeetings-avail__elem--hours-elem::before {
+      content: '\2022'; /* Add content: \2022 is the CSS Code/unicode for a bullet */
+      color: red; /* Change the color */
+      font-weight: bold; /* If you want it to be bold */
+      display: inline-block; /* Needed to add space between the bullet and the text */
+      /*width: auto;*/
+      /*margin-left: 0;*/
+      width: 150px; /* Also needed for space (tweak if needed) */
+      margin-left: -150px; /* Also needed for space (tweak if needed) */
+      font-size: 39px;
+      line-height: 0;
+    }
+  }
+}
+</style>
---
+++ components/profile/Main.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="c-account">
+    <TopMobile />
+    <div class="c-account__wrapper-section">
+      <AccountProfile />
+    </div>
+    <BottomMobile />
+  </section>
+</template>
+
+<script>
+import AccountProfile from '~/components/account/AccountProfile'
+import TopMobile from '~/components/site/TopMobile'
+import BottomMobile from '~/components/site/BottomMobile'
+
+export default {
+  name: 'Network',
+  components: {
+    AccountProfile,
+    TopMobile,
+    BottomMobile
+  },
+  data: () => ({}),
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.c-account {
+  width: 100%;
+  height: 100%;
+  background-color: #fdfdfd;
+  &__wrapper-section {
+    width: 100%;
+    padding: 25px;
+    display: flex;
+  }
+  &__right-cont {
+    width: 40%;
+    margin-left: 12px;
+  }
+  &__left-cont {
+    width: 60%;
+    margin-right: 12px;
+  }
+}
+@media screen and (max-width: 768px) {
+  .c-account {
+    &__wrapper-section {
+      flex-flow: column;
+      padding: 25px 25px 80px 25px;
+    }
+    &__right-cont {
+      width: 100%;
+      margin: 0;
+    }
+    &__left-cont {
+      width: 100%;
+      margin: 0 0 15px 0;
+    }
+  }
+}
+@media screen and (max-width: 400px) {
+  .c-account {
+    &__wrapper-section {
+      padding: 25px;
+    }
+  }
+}
+</style>
