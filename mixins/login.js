60f485709992b6ba0f215eb3de25d41b4f7d2bbf
David Gonzalez | Refineria Web <dgonzalez@refineriaweb.com>
2020-07-22 12:26:43 +0200

    Catch error login mixin

--- mixins/login.js
+++ mixins/login.js
@@ -11,40 +11,69 @@ export const login = {
      * @param application
      */
     async handleLogin(words, registerNick, registerPassword, application) {
-      if (words) {
-        // eslint-disable-next-line no-unused-vars
-        let g2cLoginResponse = null
+      try {
+        if (words) {
+          // eslint-disable-next-line no-unused-vars
+          let g2cLoginResponse = null
 
-        g2cLoginResponse = await this.loginUser(
-          words,
-          application,
-          registerNick
-        )
+          g2cLoginResponse = await this.loginUser(
+            words,
+            application,
+            registerNick
+          )
 
-        if (!g2cLoginResponse.error) {
-          await this.$auth
-            .loginWith('g2c_user', {
+          if (!g2cLoginResponse.error) {
+            await this.$auth
+              .loginWith('g2c_user', {
+                data: {
+                  nick: registerNick,
+                  password: registerPassword
+                }
+              })
+              .then(() => {
+                this.$router.push('/home')
+              })
+              .catch((error) => {
+                this.handleError(error, 'G2CUser@logoutUser - Error')
+                throw error
+              })
+          }
+        } else {
+          this.$auth
+            .loginWith('user', {
               data: {
                 nick: registerNick,
                 password: registerPassword
               }
             })
             .then(() => {
-              this.$router.push('/home')
+              this.$router.push(this.localePath('/user-profile'))
+            })
+            .catch((error) => {
+              this.handleError(error, 'G2CUser@loginUser - Error')
+              throw error
             })
         }
-      } else {
-        this.$auth
-          .loginWith('user', {
-            data: {
-              nick: registerNick,
-              password: registerPassword
-            }
-          })
-          .then(() => {
-            this.$router.push(this.localePath('/user-profile'))
-          })
+      } catch (error) {
+        this.handleError(error, 'G2CUser@loginUser3 - Error')
+        throw error
       }
+    },
+    /**
+     * Error Handler
+     * @param {Error} error
+     * @param {string} title - Optional Title
+     */
+    handleError(error, title = '') {
+      if (title.length > 0) {
+        // eslint-disable-next-line no-console
+        console.error(title)
+      }
+
+      // eslint-disable-next-line no-console
+      console.error(error)
+      this.errorValidation = 'login fail'
+      this.loading = false
     }
   }
 }
